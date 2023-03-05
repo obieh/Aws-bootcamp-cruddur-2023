717f3979144b0093fe2a5f92f8147165d617ef6c
Obinna Chikere Amadi <o_amadic@yahoo.com>
2023-03-05 01:05:08 +0000

    week-3

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -22,72 +22,73 @@ export default function SigninPage() {
           localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
           window.location.href = "/"
         })
-        .catch(err => { console.log('Error!', err) });
-    } catch (error) {
-      if (error.code == 'UserNotConfirmedException') {
-        window.location.href = "/confirm"
-      }
-      setErrors(error.message)
+        .catch(console.error(); => {
+    if (error.code == 'UserNotConfirmedException') {
+      window.location.href = "/confirm"
     }
-    return false
+    setErrors(error.message)
+  });
+  return false
+}
+    
   }
 
-  const email_onchange = (event) => {
-    setEmail(event.target.value);
-  }
-  const password_onchange = (event) => {
-    setPassword(event.target.value);
-  }
+const email_onchange = (event) => {
+  setEmail(event.target.value);
+}
+const password_onchange = (event) => {
+  setPassword(event.target.value);
+}
 
-  let el_errors;
-  if (errors) {
-    el_errors = <div className='errors'>{errors}</div>;
-  }
+let el_errors;
+if (errors) {
+  el_errors = <div className='errors'>{errors}</div>;
+}
 
-  return (
-    <article className="signin-article">
-      <div className='signin-info'>
-        <Logo className='logo' />
-      </div>
-      <div className='signin-wrapper'>
-        <form
-          className='signin_form'
-          onSubmit={onsubmit}
-        >
-          <h2>Sign into your Cruddur account</h2>
-          <div className='fields'>
-            <div className='field text_field username'>
-              <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={email_onchange}
-              />
-            </div>
-            <div className='field text_field password'>
-              <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={password_onchange}
-              />
-            </div>
+return (
+  <article className="signin-article">
+    <div className='signin-info'>
+      <Logo className='logo' />
+    </div>
+    <div className='signin-wrapper'>
+      <form
+        className='signin_form'
+        onSubmit={onsubmit}
+      >
+        <h2>Sign into your Cruddur account</h2>
+        <div className='fields'>
+          <div className='field text_field username'>
+            <label>Email</label>
+            <input
+              type="text"
+              value={email}
+              onChange={email_onchange}
+            />
           </div>
-          {el_errors}
-          <div className='submit'>
-            <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
-            <button type='submit'>Sign In</button>
+          <div className='field text_field password'>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={password_onchange}
+            />
           </div>
-
-        </form>
-        <div className="dont-have-an-account">
-          <span>
-            Don't have an account?
-          </span>
-          <Link to="/signup">Sign up!</Link>
         </div>
-      </div>
+        {el_errors}
+        <div className='submit'>
+          <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
+          <button type='submit'>Sign In</button>
+        </div>
 
-    </article>
-  );
+      </form>
+      <div className="dont-have-an-account">
+        <span>
+          Don't have an account?
+        </span>
+        <Link to="/signup">Sign up!</Link>
+      </div>
+    </div>
+
+  </article>
+);
 }
